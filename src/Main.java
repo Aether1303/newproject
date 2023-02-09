79437103c83c6f6908cdc4ac8a281204525637c5
Aether1303 <aathiragreen@gmail.com>
2023-02-09 16:16:31 +0400

    no change testing git

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    int adde(int x,int y){
+        System.out.println(x-y);
+        return 0;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
