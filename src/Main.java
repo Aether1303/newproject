b5d46ed070ea375f943c8183a7680f693699afec
Aether1303 <aathiragreen@gmail.com>
2023-02-09 16:20:03 +0400

    add

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     int adde(int x,int y){
-        System.out.println(x-y);
+        System.out.println(x+y);
         return 0;
     }
     public static void main(String[] args) {
